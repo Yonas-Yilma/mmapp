a33cd7a4f09f65bb29d2fff569d2872b398c6e00
Yonas-Yilma <yilmayonas7@gmail.com>
2025-04-11 23:01:28 -0700

    Add files via upload

---
+++ MOM/3cn.js
@@ -0,0 +1,13 @@
+/*
+let count_app=document.getElementById("count_app");
+let count=0;
+function addValue(){
+    count++;
+    count_app.innerText=count;
+
+    console.log(count);
+
+}
+    */
+  //console.log(typeof document);
+  console.log(typeof document);
---
+++ MOM/jav.js
@@ -0,0 +1,24 @@
+//console.log(`Totalcost=$${3 + 5}`);
+  /*function add(a,b){
+    if(a===b){
+        return 3*(a+b);
+    }
+    else{
+        return a+b;
+    }
+}
+let good=add(7,8);
+console.log(good);
+*/
+let a=478;
+let b=748;
+
+if(a.toString()[1]===b.toString()[0]){
+    console.log("both are the same");
+}
+else{
+    console.log("no");
+}
+
+
+
